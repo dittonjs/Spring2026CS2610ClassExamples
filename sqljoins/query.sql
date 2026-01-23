bf5cd15a329ed0a9abe70a6116401d8cb100924b
Joseph Ditton <jditton.atomic@gmail.com>
2026-01-23 11:31:12 -0700

    class

--- sqljoins/query.sql
+++ sqljoins/query.sql
@@ -1,7 +1,38 @@
-SELECT customer.id, max(purchase_order.amount)
---
-FROM customer
-INNER JOIN purchase_order ON (customer.id = purchase_order.customer_id)
-GROUP BY customer.id
---
+WITH purchase_order_totals AS (
+  SELECT customer.id, customer.name, SUM(purchase_order.amount) as total
+  FROM customer
+  INNER JOIN purchase_order ON (customer.id = purchase_order.customer_id)
+  GROUP BY customer.id
+  ORDER BY customer.name ASC
+)
+SELECT name, total
+FROM purchase_order_totals
+WHERE total = (
+  SELECT MAX(total)
+  FROM purchase_order_totals
+)
 ;
+
+
+
+
+-- SELECT name, total
+-- FROM (
+--     SELECT customer.id, customer.name, SUM(purchase_order.amount) as total
+--     FROM customer
+--     INNER JOIN purchase_order ON (customer.id = purchase_order.customer_id)
+--     GROUP BY customer.id
+--     ORDER BY customer.name ASC
+--   )
+-- WHERE total = (
+--   SELECT MAX(total)
+--   FROM (
+--     SELECT customer.id, customer.name, SUM(purchase_order.amount) as total
+--     FROM customer
+--     INNER JOIN purchase_order ON (customer.id = purchase_order.customer_id)
+--     GROUP BY customer.id
+--     ORDER BY customer.name ASC
+--   )
+-- )
+-- ;
+
---
+++ stockkeepdatabase/insert_customers.sql
@@ -0,0 +1,4 @@
+INSERT INTO customers (name, email)
+VALUES
+  ('Abby Zollinger', 'hello@example.com'),
+  ('Bruce Young', 'bruce@example.com');
